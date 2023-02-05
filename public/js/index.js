6b5d503394e6f61a43b26f85bbe3a0d15a09e547
athena272 <guilhermera272@gmail.com>
2023-02-05 17:19:55 -0300

    Create index.js

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+const frase = $(".frase").text() 
+const qtdPalavras = frase.trim().split(" ")
+
+console.log(qtdPalavras)
+
+// let str = "   Hello, World!   ";
+// str = str.trim();
+// console.log(str);  // Output: "Hello, World!"
