0bc60a670b05bf29ca5a7795fb6b652e7736ddcb
athena272 <guilhermera272@gmail.com>
2023-02-05 20:26:31 -0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -12,7 +12,8 @@ tamanhoFrase.text(qtdPalavras)
 const campoDigitacao = $(".campo-digitacao")
 campoDigitacao.on("input", function() {
     const content = campoDigitacao.val()
-    
+
+    // Regular expression to remove backspace
     const qtdPalavrasCampo = content.split(/\S+/).length - 1
     $('#contador-palavras').text(qtdPalavrasCampo)
 
