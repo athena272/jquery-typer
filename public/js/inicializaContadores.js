83562bb248b1bfbbf16dbb8bffe9916859c6fb6f
athena272 <guilhermera272@gmail.com>
2023-02-07 21:05:48 -0300

    Create inicializaContadores.js

---
+++ public/js/inicializaContadores.js
@@ -0,0 +1,14 @@
+const campoDigitacao = $(".campo-digitacao")
+
+export function inicializaContadores() {
+    campoDigitacao.on("input", function () {
+        const content = campoDigitacao.val()
+
+        // Regular expression to remove backspace
+        const qtdPalavrasCampo = content.split(/\S+/).length - 1
+        $('#contador-palavras').text(qtdPalavrasCampo)
+
+        const qtdCaracteresCampo = content.length
+        $('#contador-caracteres').text(qtdCaracteresCampo)
+    })
+}
