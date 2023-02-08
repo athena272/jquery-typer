4ee9857ba5b53084d72391b0df12b11a86236564
athena272 <guilhermera272@gmail.com>
2023-02-07 21:05:53 -0300

    Create tamanhoTexto.js

---
+++ public/js/tamanhoTexto.js
@@ -0,0 +1,12 @@
+// .text() sem parametro retorna o valor textual do elemento
+// .text(param) com parametro seta o valor textual no elemento
+
+// Capturar tamanho da palavra e caracteres do campo de digitacao
+export function tamanhoTexto() {
+    const texto = $(".texto").text()
+    const palavras = texto.trim().split(" ")
+    const qtdPalavras = palavras.length
+
+    const tamanhoFrase = $("#tamanho-texto")
+    tamanhoFrase.text(qtdPalavras)
+}
