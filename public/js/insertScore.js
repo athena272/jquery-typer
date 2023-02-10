fe5c2b0061b4a99a5bf6c6e28b0d387fa41a172e
athena272 <guilhermera272@gmail.com>
2023-02-10 16:39:13 -0300

    Update insertScore.js

--- public/js/insertScore.js
+++ public/js/insertScore.js
@@ -2,14 +2,43 @@ export function insertScore() {
     const corpoTabela = $(".placar").find("tbody")
     const userName = "userName"
     const qtdPalavras = $('#contador-palavras').text()
-    const removeBtn = `<a href="#"><i class="small material-icons">delete</i></a>`
+    // const removeBtn = `<a href="#"><i class="small material-icons">delete</i></a>`
+    // `<tr>` +
+    //     `<td> ${userName} </td>` +
+    //     `<td> ${qtdPalavras} </td>` +
+    //     `<td> ${removeBtn} </td>` +
+    //     `</tr>`
 
-    const newLine = `<tr>` +
-        `<td> ${userName} </td>` +
-        `<td> ${qtdPalavras} </td>` +
-        `<td> ${removeBtn} </td>` +
-        `</tr>`
+    const newLineTable = newLine(userName, qtdPalavras)
+    newLineTable.find(".botao-remover").click(removeLine)
 
-    corpoTabela.prepend(newLine)
+    corpoTabela.prepend(newLineTable)
     // corpoTabela.append(newLine)
 }
+
+function newLine(userName, qtdPalavras) {
+    const line = $("<tr>")
+    const columnUser = $("<td>").text(userName)
+    const columnQtdPalavras = $("<td>").text(qtdPalavras)
+    const columnBtnRemove = $("<td>")
+
+    const link = $("<a>").attr("href", "#").addClass("botao-remover")
+    const icon = $("<i>").addClass("small").addClass("material-icons").text("delete")
+
+    // Icone dentro do <a>
+    link.append(icon);
+
+    // <a> dentro do <td>
+    columnBtnRemove.append(link);
+
+    // Os três <td> dentro do <tr>
+    line.append(columnUser, columnQtdPalavras, columnBtnRemove)
+
+    return line
+}
+
+function removeLine(ev) {
+    ev.preventDefault()
+    $(this).parent().parent().remove()
+}
+
