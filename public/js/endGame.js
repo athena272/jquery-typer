54f5bd27f947c2aaf6659be01ebfa7c9c997ae46
athena272 <guilhermera272@gmail.com>
2023-02-10 09:17:36 -0300

    Create endGame.js

---
+++ public/js/endGame.js
@@ -0,0 +1,9 @@
+const campoDigitacao = $(".campo-digitacao")
+
+export function endGame() {
+    campoDigitacao.attr("disabled", true)
+
+    $("#reiniciar-game").attr("disabled", false)
+    campoDigitacao.addClass("campo-desabilitado")
+    // campoDigitacao.toggleClass("campo-desabilitado")
+}
