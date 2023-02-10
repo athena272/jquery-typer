d87e30aa41db4b5a8f5b550c971ec5552ca73e65
athena272 <guilhermera272@gmail.com>
2023-02-10 14:45:28 -0300

    Update insertScore.js

--- public/js/insertScore.js
+++ public/js/insertScore.js
@@ -2,10 +2,12 @@ export function insertScore() {
     const corpoTabela = $(".placar").find("tbody")
     const userName = "userName"
     const qtdPalavras = $('#contador-palavras').text()
+    const removeBtn = `<a href="#"><i class="small material-icons">delete</i></a>`
 
     const newLine = `<tr>` +
         `<td> ${userName} </td>` +
         `<td> ${qtdPalavras} </td>` +
+        `<td> ${removeBtn} </td>` +
         `</tr>`
 
     corpoTabela.prepend(newLine)
