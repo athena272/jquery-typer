453ced49ee07f3246ad2181e97674718f75c5f9d
athena272 <guilhermera272@gmail.com>
2023-02-10 09:05:12 -0300

    Create checkText.js

---
+++ public/js/checkText.js
@@ -0,0 +1,30 @@
+const texto = $(".texto").text().trim()
+const campoDigitacao = $(".campo-digitacao")
+
+export function checkText() {
+    campoDigitacao.on("input", function () {
+        const digitado = campoDigitacao.val()
+        // const pedacoComparavel = texto.substr(0, digitado.length)
+        // MODERN WAY
+        const digitadoCorretamente = texto.startsWith(digitado)
+      
+        if (digitadoCorretamente) {
+            console.log("VERDE")
+            campoDigitacao.addClass("borda-verde")
+            campoDigitacao.removeClass("borda-vermelha")
+
+        } else {
+            console.log("Vermelho")
+
+            campoDigitacao.addClass("borda-vermelha")
+            campoDigitacao.removeClass("borda-verde")
+
+        }
+        // ANOTHER WAY
+        // var ehCorreto = (digitado === pedacoComparavel);
+
+        // campo.toggleClass("borda-verde", ehCorreto);
+        // campo.toggleClass("borda-vermelha", !ehCorreto);
+
+    })
+}
